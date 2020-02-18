12b8a58e9d79fbd38444d88a6b1e7d6576350b8f
Witold Kuśnierz <WITKU@statoilfuelretail.com>
2020-02-18 16:57:27 +0100

    additional

--- terraform/modules/bi_data_lake_module/main.tf
+++ terraform/modules/bi_data_lake_module/main.tf
@@ -43,10 +43,48 @@ module "dl_databricks" {
 }
 
 # SSAS Instance (1 by default)
+module "dl_ssas" {
+  source                            = "./ssas"
+  subscription_id					= var.subscription_id
+  tenant_id                         = var.tenant_id
+  bi_env_prefix                     = var.bi_env_prefix
+  bi_env_suffix                     = var.bi_env_suffix
+  bi_resource_group_prm             = var.bi_resource_group_prm
+  bi_resource_group_scnd            = var.bi_resource_group_scnd
+  bi_location_prm                   = var.bi_location_prm
+  bi_location_scnd                  = var.bi_location_scnd
+  tags                              = var.tags
+}
 
 # Azure Data Factory (1 by default)
+module "dl_adf" {
+  source                            = "./adf"
+  subscription_id					= var.subscription_id
+  tenant_id                         = var.tenant_id
+  bi_env_prefix                     = var.bi_env_prefix
+  bi_env_suffix                     = var.bi_env_suffix
+  bi_resource_group_prm             = var.bi_resource_group_prm
+  bi_resource_group_scnd            = var.bi_resource_group_scnd
+  bi_location_prm                   = var.bi_location_prm
+  bi_location_scnd                  = var.bi_location_scnd
+  tags                              = var.tags
+}
 
 # Key Vaults (2, including initial secrets setup)
+module "dl_key_vaults" {
+  source                            = "./ssas"
+  subscription_id					= var.subscription_id
+  tenant_id                         = var.tenant_id
+  bi_env_prefix                     = var.bi_env_prefix
+  bi_env_suffix                     = var.bi_env_suffix
+  bi_resource_group_prm             = var.bi_resource_group_prm
+  bi_resource_group_scnd            = var.bi_resource_group_scnd
+  bi_location_prm                   = var.bi_location_prm
+  bi_location_scnd                  = var.bi_location_scnd
+  tags                              = var.tags
+  
+  depends_on = module.dl_storage
+}
 
 # VM (1 VM hosting SQL Server, MDS, Power BI Gateway and Azure Data Factory Integration Runtime)
 
